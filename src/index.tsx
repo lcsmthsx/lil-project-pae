261dba001108e3b4a8dd426e2bff96a5f9e1f4dd
Lucas Matheus <lucasmcontato@outlook.com>
2021-06-30 10:54:32 -0300

    Preparing the environment for the new project

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import ReactDOM from "react-dom";
-import Button from "./Button";
-import Icon from "./Icon";
-import Icon3 from "./IconThree"
-import Icon2 from "./IconTwo";
-import ColoredSquare from "./ColoredSquare";
+
 
 function App(){
-  const [color, setColor]= 
-  useState("grey")
 
-  return( <div>
-    <ColoredSquare color={color}/>
-            <Button clicked={()=>setColor("black")} >
-              <Icon />
-              <p>BLACK</p>
-            </Button>
-            <Button clicked={()=>setColor("red")} >
-              <Icon2 />
-              <p>RED</p>
-            </Button>
-            <Button clicked={()=>setColor("blue")}>
-              <Icon3 />
-              <p>BLUE</p>
-            </Button>
-          </div>
-        );
+
+  return <>
+    <h1>bom dia</h1>
+  </>
+        
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
