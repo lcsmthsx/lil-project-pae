5036d6a4134d02f39f2ade346337e680a0eae29e
Lucas Matheus <lucasmcontato@outlook.com>
2021-07-01 00:12:29 -0300

    Implemented Dark mode with useState

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css"
+
+//☾☀
+function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  return (<>
+
+    <div className={darkMode ? 'dark-mode' : 'light-mode'}>
+      <div className="container">
+        <span style={{ color: darkMode ? 'grey' : "yellow" }}>☀</span>
+        <div className="switch-checkbox">
+          <label className="switch">
+            <input type="checkbox" onChange={() => setDarkMode(!darkMode)} />
+            <span className="slider round"> </span>
+          </label>
+        </div>
+        <span style={{ color: darkMode ? '#c96dfd' : "grey" }}>☾</span>
+      </div>
+      <div>
+        <h1>Massa, tá no {darkMode ? "Dark" : "Light"} Mode </h1>
+      </div>
+    </div>
+
+
+  </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
